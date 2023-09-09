867be466153798f8bd3f8da0398b3d3992cdf559
DAOUDI Mohammed <110437117+7emoutyX@users.noreply.github.com>
2023-09-09 02:23:36 +0100

    Add files via upload

---
+++ cmake-build-nodemcu-32s/CMakeFiles/Z_DUMMY_TARGET.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Hemouty/CLionProjects/b3bola/.pio/libdeps/nodemcu-32s/Adafruit TSL2561/Adafruit_TSL2561_U.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/Adafruit_TSL2561/Adafruit_TSL2561_U.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/Adafruit_TSL2561/Adafruit_TSL2561_U.cpp.obj.d"
+  "C:/Users/Hemouty/CLionProjects/b3bola/.pio/libdeps/nodemcu-32s/Adafruit Unified Sensor/Adafruit_Sensor.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/Adafruit_Unified_Sensor/Adafruit_Sensor.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/Adafruit_Unified_Sensor/Adafruit_Sensor.cpp.obj.d"
+  "C:/Users/Hemouty/CLionProjects/b3bola/.pio/libdeps/nodemcu-32s/AsyncTCP-esphome/src/AsyncTCP.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/AsyncTCP-esphome/src/AsyncTCP.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/AsyncTCP-esphome/src/AsyncTCP.cpp.obj.d"
+  "C:/Users/Hemouty/CLionProjects/b3bola/.pio/libdeps/nodemcu-32s/ESP32WebServer/src/ESP32WebServer.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESP32WebServer/src/ESP32WebServer.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESP32WebServer/src/ESP32WebServer.cpp.obj.d"
+  "C:/Users/Hemouty/CLionProjects/b3bola/.pio/libdeps/nodemcu-32s/ESP32WebServer/src/Parsing.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESP32WebServer/src/Parsing.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESP32WebServer/src/Parsing.cpp.obj.d"
+  "C:/Users/Hemouty/CLionProjects/b3bola/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/AsyncEventSource.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/AsyncEventSource.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/AsyncEventSource.cpp.obj.d"
+  "C:/Users/Hemouty/CLionProjects/b3bola/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/AsyncWebSocket.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/AsyncWebSocket.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/AsyncWebSocket.cpp.obj.d"
+  "C:/Users/Hemouty/CLionProjects/b3bola/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebAuthentication.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebAuthentication.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebAuthentication.cpp.obj.d"
+  "C:/Users/Hemouty/CLionProjects/b3bola/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebHandlers.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebHandlers.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebHandlers.cpp.obj.d"
+  "C:/Users/Hemouty/CLionProjects/b3bola/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebRequest.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebRequest.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebRequest.cpp.obj.d"
+  "C:/Users/Hemouty/CLionProjects/b3bola/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebResponses.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebResponses.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebResponses.cpp.obj.d"
+  "C:/Users/Hemouty/CLionProjects/b3bola/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebServer.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebServer.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebServer.cpp.obj.d"
+  "C:/Users/Hemouty/CLionProjects/b3bola/src/main.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/src/main.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-nodemcu-32s/CMakeFiles/Z_DUMMY_TARGET.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/Adafruit_TSL2561/Adafruit_TSL2561_U.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/Adafruit_TSL2561/Adafruit_TSL2561_U.cpp.obj.d"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/Adafruit_Unified_Sensor/Adafruit_Sensor.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/Adafruit_Unified_Sensor/Adafruit_Sensor.cpp.obj.d"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/AsyncTCP-esphome/src/AsyncTCP.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/AsyncTCP-esphome/src/AsyncTCP.cpp.obj.d"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESP32WebServer/src/ESP32WebServer.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESP32WebServer/src/ESP32WebServer.cpp.obj.d"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESP32WebServer/src/Parsing.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESP32WebServer/src/Parsing.cpp.obj.d"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/AsyncEventSource.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/AsyncEventSource.cpp.obj.d"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/AsyncWebSocket.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/AsyncWebSocket.cpp.obj.d"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebAuthentication.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebAuthentication.cpp.obj.d"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebHandlers.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebHandlers.cpp.obj.d"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebRequest.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebRequest.cpp.obj.d"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebResponses.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebResponses.cpp.obj.d"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebServer.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/nodemcu-32s/ESPAsyncWebServer-esphome/src/WebServer.cpp.obj.d"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/main.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/main.cpp.obj.d"
+  "Z_DUMMY_TARGET"
+  "Z_DUMMY_TARGET.manifest"
+  "Z_DUMMY_TARGET.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Z_DUMMY_TARGET.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
